e1ba626cb2b253cd01c0bd6da4720511c14763f1
majdifox <majmehdi20@gmail.com>
2024-12-24 17:16:27 +0100

    user class

--- hopital.php
+++ hopital.php
@@ -1,29 +1,46 @@
 <?php
 
-class cars {
+class user {
 
-    // atrributes (is like the sources that we're going to use)
 
-    private $brand;
-    private $color;
+    public $nom;
+    public $prenom;
+    public  $type_utilisateur;
 
-    // methods
-    // the first step is to use the constructor (by default is already there but we shoud intialize it first)
-    // we should add some parameters on the function of the constructor 
 
-    public function __construct($brand = null , $color = null){
+    public function __construct($nom = null, $prenom = null, $type_utilisateur = null){
 
-        // the first brand should be as same as the assigned variable (private $brand);
-        // but the the last brand you can name whatever you want it's just a variable 
-        $this -> brand = $brand;
+        $this -> nom = $nom;
+        $this -> prenom = $prenom;
+        $this -> type_utilisateur = $type_utilisateur;
+    
 
-        $this -> color = $color;
+    }
+    
+    public function afficherNomComplet() {
+        return $this->prenom . " " . $this->nom;
+    }
 
+    
+    public function changerNom($nouveauNom) {
+        $this->nom = $nouveauNom;
+    }
+
+
+    public function changerPrenom($nouveauPrenom) {
+        $this->prenom = $nouveauPrenom;
     }
 
 }
 
 
+$name = new user("Abdellah","EL MALKY");
+
+
+var_dump($name);
+
+
+echo 'it works';
 
 
 ?>
--- index.php
+++ index.php
@@ -8,19 +8,6 @@ require_once 'hopital.php';
 //  to create objets for the class cars.php we use instanciation
 
 
-// here we're going to create an example car1 is an object 
-$car1 = new cars(color:"bugatti", brand:"red");
-
-
-$car2 = new cars(brand:"Ferrari", color:"red");
-
-// to diplay the value of car1 we use a funtion called var_dump() here we go
-
-var_dump($car1, $car2);
-
-// var_dump($car2);
-
-echo 'it works';
 
 
 ?>
